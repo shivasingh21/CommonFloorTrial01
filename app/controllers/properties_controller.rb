9e45bfbcd022be4f3b42e1d1d5cbcc71b22c3e9e
shivasingh21 <shivat517@gmail.com>
2020-12-13 14:00:12 +0530

    Submit-2

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -7,10 +7,22 @@ class PropertiesController < ApplicationController
     @property = Property.new
   end
 
+  def show
+    @property = Property.find(params[:id])
+  end
+
   def create
     @property = Property.new(property_params)
+    @property.user = User.first
     if @property.save
-      redirect_to 'root'
+      redirect_to @property
+    else
+      render 'new'
     end
   end
+
+  private
+    def property_params
+      params.require(:property).permit(:PropertyType, :Location, :Price, :Area, :OwnerName, :ContactPerson, :Number)
+    end
 end
