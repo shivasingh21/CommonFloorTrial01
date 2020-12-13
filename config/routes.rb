46c5cd64e1713ae5f84a7d39f0442c846bcb0029
shivasingh21 <shivat517@gmail.com>
2020-12-14 01:53:30 +0530

    submit-3

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
   resources :properties
+  get 'signup', to: 'users#new'
+  post 'users', to: 'users#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
